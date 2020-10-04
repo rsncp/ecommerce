89bfaf1f2fb825bed6354c37e5dbb122cb550724
Ajmal Roshan <mohroshanajmal@gmail.com>
2020-10-04 20:51:21 +0530

    login time

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 var express = require('express');
+const { response } = require('../app');
 var router = express.Router();
 const productHelper=require('../helpers/product-helpers')
+const userHelpers=require('../helpers/user-helpres')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,5 +20,16 @@ router.get('/', function(req, res, next) {
 router.get('/login',(req,res)=>{
  res.render('user/login')
 })
+router.get('/signup',(req,res)=>{
+  res.render('user/signup')
+})
+router.post('/signup',(req,res)=>{
+  userHelpers.doSignup(req.body).then((response)=>{
+    console.log(response);
+  })
+})
 
+router.post('/login',(req,res)=>{
+  userHelpers.doLogin(req.body)
+})
 module.exports = router;
